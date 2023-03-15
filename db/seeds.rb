18e615bf48ee8f57726be0002ff965a7e2cc684e
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-15 16:09:36 +0100

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+osaka = {name: "Osaka", address: "Rue jean Jaurès", category: "japanese"}
+kenavo = {name: "Kenavo", address: "Crozon", category: "french"}
+twatwaffle = {name: "Twatwaffle", address: "quelque part à Bruxelles", category: "belgian"}
+
+[dishoom, pizza_east, osaka, kenavo, twatwaffle].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
